558e397f62a0e9f838b32f6850a7616583d17230
Quadtree <quadtree@gmail.com>
2017-04-21 21:30:03 -0700

    Checkpoints added

---
+++ Source/LD38/Checkpoint.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "Checkpoint.generated.h"
+
+UCLASS()
+class LD38_API ACheckpoint : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ACheckpoint();
+
+	UPROPERTY(EditAnywhere, Category = Checkpoint)
+	int32 CheckpointNumber;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	
+	
+};
